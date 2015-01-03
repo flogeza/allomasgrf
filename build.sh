99a3f7f795417a0df3542126f5e041d344f30106
idioty <idioty@freemail.hu>
2015-01-03 22:02:20 +0100

    Advanced Sprite Layout tanulmányozása.

--- build.sh
+++ build.sh
@@ -108,100 +108,12 @@ if [[ $aret_code -gt $ret_code ]]; then
 	ret_code=$aret_code
 fi
 
+eval creategrf "$1" "$SRCROOT" "$SRCROOT/sprites" "$SRCROOT/nsb" "hsst" "hungarian_stations_teszt" "$lastbuildfiledate" "/Users/idioty/Documents/OpenTTD/newgrf" 2>&1
+aret_code=$?
+if [[ $aret_code -gt $ret_code ]]; then
+	ret_code=$aret_code
+fi
 
-
-
-#eval creategrf "$1" "$SRCROOT" "$SRCROOT/sprites" "$SRCROOT/nsb" "hsst" "hungarian_stations_teszt" "$lastbuildfiledate" "/Users/idioty/Documents/OpenTTD/newgrf" 2>&1
-#aret_code=$?
-#if [[ $aret_code -gt $ret_code ]]; then
-#	ret_code=$aret_code
-#fi
-#
-
-
-
-
-#echo "barmi" > "$lastbuildfile"
-##exit 0
-#
-#cd $SRCROOT
-#
-## nfo osszrekasa, ellenorzese
-#sajatgrfname="hungarian_stations"
-#sajatgrftestname=$sajatgrfname"_test"
-#sajatnfoname=$sajatgrfname".nfo"
-#sajatnfo="sprites/"$sajatnfoname
-#sajatnfotestname=$sajatgrftestname".nfo"
-#sajatnfotest="sprites/"$sajatnfotestname
-#
-#if [[ -f "$sajatnfotest" ]]; then
-#	rm "$sajatnfotest"
-#fi
-#
-#while read -r line
-#do
-#	echo "line: "$line
-#	if [[ ! "$line" = "$sajatnfo" ]] && [[ ! "$line" = "$sajatnfotest" ]]; then
-#		cat "$line" >> "$sajatnfotest"
-#	fi
-#done < <(find sprites -name "*nfo" -type f -mindepth 1 -maxdepth 1)
-#
-#eval "nforenum $sajatnfotestname 2>&1"
-#ret_code=$?
-#if [ $ret_code == 0 ]; then
-#	echo "NFRRENUM sikeresen lefutott"
-#	grftestneve=$sajatgrftestname".grf"
-#	eval "grfcodec -e $grftestneve 2>&1"
-#	ret_code=$?
-#	if [ $ret_code == 0 ]; then
-#		echo "GRFMAKER sikeresen lefutott"
-#
-#		if [[ -f /Users/idioty/Documents/OpenTTD/newgrf/"$grftestneve" ]]; then
-#			rm /Users/idioty/Documents/OpenTTD/newgrf/"$grftestneve"
-#		fi
-#		cp "$grftestneve" /Users/idioty/Documents/OpenTTD/newgrf/
-#
-#
-#		# most elkeszult a tesztverzio ami nekem fut a gepemen, es most megcsinaljuk a teszt nelkuli kiadast
-#		# egesz teszt kihagyasa
-#		exlude="! -name 99_teszt.nfo"
-#
-#		# egesz nyugati kihagyasa
-#		exlude=$exlude" ! -name 1*.nfo"
-#
-#		if [[ -f "$sajatnfo" ]]; then
-#			rm "$sajatnfo"
-#		fi
-#
-#		while read -r line
-#		do
-#			if [[ ! "$line" = "$sajatnfo" ]] && [[ ! "$line" = "$sajatnfotest" ]]; then
-#				cat "$line" >> "$sajatnfo"
-#			fi
-#		done < <(find sprites -name "*nfo" ! -name "$sajatnfotest" ! -name "sajatnfo" $exlude -type f -mindepth 1 -maxdepth 1)
-#
-#		eval "nforenum $sajatnfoname 2>&1"
-#		ret_code=$?
-#		if [ $ret_code == 0 ]; then
-#			grfneve=$sajatgrfname".grf"
-#			eval "grfcodec -e $grfneve 2>&1"
-#			ret_code=$?
-#			if [ $ret_code == 0 ]; then
-#				echo "GRFMAKER sikeresen lefutott a release grffel"
-#			else
-#				echo "Hiba van a GRFMAKERREL a release grf elkészítésekor"
-#			fi
-#		else
-#			echo "Hiba van az NFORENUMMAL a release nfo elkészítésekor"
-#		fi
-#	else
-#		echo "Hiba van a GRFMAKERREL"
-#	fi
-#else
-#	echo "Hiba van az NFORENUMMAL"
-#fi
-#
-#echo "barmi" > "$lastbuildfile"
 
 exit $ret_code
 #IFS=$SAVEIFS
